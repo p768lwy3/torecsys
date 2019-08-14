17e806b532fc93646dcb0b4e674be4dbcdc6b4b3
jasper <p768lwy3@gmail.com>
2019-08-14 22:33:33 +0800

    fixed estimators.fm bugs and checked jit-compatibility

--- torecsys/models/ctr/estimators/factorization_machine.py
+++ torecsys/models/ctr/estimators/factorization_machine.py
@@ -1,5 +1,6 @@
 from . import _CtrEstimator
 from ..layers import FactorizationMachineLayer
+from torecsys.utils.logging.decorator import jit_experimental
 from functools import partial
 import torch
 import torch.nn as nn
@@ -15,10 +16,12 @@ class FactorizationMachine(_CtrEstimator):
     #. `Steffen Rendle, 2010. Factorization Machine <https://www.csie.ntu.edu.tw/~b97053/paper/Rendle2010FM.pdf>`_.
 
     """
+    @jit_experimental
     def __init__(self, 
                  embed_size    : int,
                  num_fields    : int,
                  dropout_p     : float = 0.1,
+                 bias          : bool = True,
                  output_method : str = "concatenate",
                  output_size   : int = 1):
         r"""initialize Factorization Machine Estimator
@@ -34,17 +37,17 @@ class FactorizationMachine(_CtrEstimator):
         if bias:
             # output size = fm output size + linear output size + bias
             self.bias = nn.Parameter(torch.zeros(1))
-            nn.init.xavier_uniform_(self.bias.data)
-            output_size = embed_size + num_fields + 1
+            nn.init.uniform_(self.bias.data)
+            cat_size = embed_size + num_fields + 1
         else:
             # output size = fm output size + linear output size
             self.bias = None
-            output_size = embed_size + num_fields
+            cat_size = embed_size + num_fields
         
         self.fm = FactorizationMachineLayer(dropout_p)
         
         if output_method == "concatenate":
-            self.fc = nn.Linear(self.output_size, output_size)
+            self.fc = nn.Linear(cat_size, output_size)
         elif output_method == "sum":
             self.fc = partial(torch.sum, dim=1, keepdim=True)
     
@@ -80,11 +83,12 @@ class FactorizationMachine(_CtrEstimator):
 
         # repeat and reshape bias to shape = (batch size, 1)
         if self.bias is not None:
+            # shape = (batch size, number of fields * embed size + number of fields + 1)
             bias = self.bias.repeat(batch_size).view(batch_size, 1)
-
-        # cat in dim = 1
-        # shape = (batch size, number of fields * embed size + number of fields + 1)
-        outputs = torch.cat([fm_out, linear_out, self.bias], dim=1)
+            outputs = torch.cat([fm_out, linear_out, bias], dim=1)
+        else:
+            # shape = (batch size, number of fields * embed size + number of fields)
+            outputs = torch.cat([fm_out, linear_out], dim=1)
             
         # fully-connected dense layer for output, return (batch size, output size)
         # or sum with second dimension, return (batch size, 1)
