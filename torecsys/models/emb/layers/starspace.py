39b35220909728c5402df034ab3dd5b8a380807e
jasper <p768lwy3@gmail.com>
2019-08-15 20:23:26 +0800

    add new files

---
+++ torecsys/models/emb/layers/starspace.py
@@ -0,0 +1,9 @@
+import torch
+import torch as nn
+
+class StarSpaceLayer(nn.Module):
+    def __init__(self):
+        super(StarSpaceLayer, self).__init__()
+    
+    def forward(self):
+        return 
