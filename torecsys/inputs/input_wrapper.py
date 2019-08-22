f51858ab74f4990ba14743b1a1fe7f1a1a1f9d58
Jasper <jasper@ztore.com>
2019-08-22 18:22:52 +0800

    new feat: trs.inputs.InputsWrapper

--- torecsys/inputs/input_wrapper.py
+++ torecsys/inputs/input_wrapper.py
@@ -1,36 +1,63 @@
-from . import _Inputs
-from typing import Dict, List, Tuple
+from .base import _Inputs
+import torch
+from typing import Dict, List
+
 
 class InputsWrapper(_Inputs):
     def __init__(self, 
-                 schema: Dict[str, Tuple[str, _Inputs]]):
+                 schema: Dict[str, tuple]):
         r"""InputsWrapper is a wrapper to concatenate numbers of _Inputs of different fields
         
         Args:
-            schema (Dict[str, Tuple[str, _Inputs]]): schema of wrapper
+            schema (Dict[str, tuple]): dictionary of schema, where keys are names of output fields and values are tuples of embeddings function and inputs arguments,
+            e.g. ```python
+            schema = {
+                "user"  : (trs.inputs.base.SingleIndexEmbedding(4, 10), ["userId"]),
+                "movie" : (trs.inputs.base.SingleIndexEmbedding(4, 10), ["movieId"]),
+                "pair"  : (trs.inputs.base.FieldAwareMultipleIndexEmbedding(4, [10, 10]), ["userId", "movieId"]),
+                "seq"   : (trs.inputs.base.SequenceIndexEmbedding(4, 10), ["seqId"], ["seqLength"])
+            }
+            ```
         
-        Key-Values:
-            output name: tuple of schema, where the 1st element is input name and the 2nd element is the type of _Inputs in torecsys.inputs.base
         """
         super(InputsWrapper, self).__init__()
+        
         # store the schema to stack
         self.schema = schema
-
-        # store the inverse dict to get the output's field name by input's field name
-        # raise : one-to-one?
-        self.dict_inv = {v[0]: k for k, v in schema.items()}
     
-    def forward(self, inputs: Dict[str, Tuple[torch.Tensor]]):
-        # init output list to store the tensors
-        outputs = []
-        for inp_fname, tensor_tup in inputs.item():
-            # get the output field name by input field name
-            out_fname = self.dict_inv[inp_fname]
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        r"""forward process of InputsWrapper to wrap the inputs into a dictionary
+        
+        Args:
+            inputs (Dict[str, T]): inputs values or index to be passed to function in trs.inputs.base, where keys should be existed in schema defined in __init__.
+        
+        Returns:
+            Dict[str, T]: a dictionary of outputs, where keys are output field name and values are (embedding) values.
+        """
+        # init outputs dictionary to store outputs' tensor
+        outputs = dict()
 
-            # get the embedding function
-            embedding = self.schema[out_fname][1]
+        # loop through schema for each output field
+        for out_name, args_tuple in self.schema.items():
+            # get basic args
+            embedding = args_tuple[0]
+            inp_name = args_tuple[1]
+            
+            # get tensor of inputs field from inputs
+            inp_val = [inputs[i] for i in inp_name]
+            
+            # cat the tensors of inputs
+            inp_val = torch.cat(inp_val, dim=1)
+            args = [inp_val]
+            
+            # get args if type of embedding is the required type
+            if embedding.__class__.__name__ == "SequenceIndexEmbedding":
+                arg_name = args_tuple[2][0]
+                print(arg_name)
+                args.append(inputs[arg_name])
+            
+            # embedding
+            outputs[out_name] = embedding(*args)
 
-            # append the tensors to outputs
-            outputs.append(embedding(*tensor_tup))
-
-        return 
+        return outputs
+    
