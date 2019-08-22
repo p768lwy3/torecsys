1eff39637a8ab3cc8d73005760089e259096b3ef
jasper <p768lwy3@gmail.com>
2019-08-22 21:55:23 +0800

    New Feat: concat_inputs (replace org StackInputs)

--- torecsys/inputs/base/concat_inputs.py
+++ torecsys/inputs/base/concat_inputs.py
@@ -3,17 +3,30 @@ import torch
 from typing import List
 
 
-class ConcatInputs(_Inputs):
+class ConcatInputs(nn.Module):
     r"""
     """
     def __init__(self, schema: List[tuple]):
+        r"""[summary]
         
+        Args:
+            nn ([type]): [description]
+            schema (List[tuple]): [description]
+        """
         super(ConcatInputs, self).__init__()
+
         # store the schema to self
         self.schema = schema
     
-    def forward(self, inputs: List[torch.Tensor]) -> torch.Tensor:
-
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
+        r"""[summary]
+        
+        Args:
+            inputs (Dict[str, torch.Tensor]): [description]
+        
+        Returns:
+            torch.Tensor: [description]
+        """
         outputs = list()
 
         for args_tuple in self.schema:
@@ -21,3 +34,17 @@ class ConcatInputs(_Inputs):
             embedding = args_tuple[0]
             inp_name = args_tuple[1]
             
+            inp_val = [inputs[i] for i in inp_name]
+            inp_val = torch.cat(inp_val, dim=1)
+            args = [inp_val]
+            
+            if embedding.__class__.__name__ == "SequenceIndexEmbedding":
+                arg_name = args_tuple[2][0]
+                args.append(inputs[arg_name])
+            
+            outputs.append(embedding(*args))
+
+        outputs = torch.cat(outputs, dim=2)
+        
+        return outputs
+        
