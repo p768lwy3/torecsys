8be07478e685ab0b72f126cbbf240492c2881c11
Jasper <jasper@ztore.com>
2019-08-27 12:34:04 +0800

    UPDATED: loggin of trainer

--- torecsys/utils/training/trainer.py
+++ torecsys/utils/training/trainer.py
@@ -192,7 +192,7 @@ class Trainer(object):
                 # log for each y steps
                 if global_step % self.log_step == 0:
                     if self.verboses >= 1:
-                        self.logger.debug("step avg loss : %.4f" % (steps_loss / self.log_step))
+                        self.logger.debug("step avg loss at step %d of epoch %d : %.4f" % (i, epoch, steps_loss / self.log_step))
                     if self.verboses >= 2:
                         self.writer.add_scalar("training/steps_avg_loss", steps_loss / self.log_step, global_step=global_step)    
                     steps_loss = 0.0
