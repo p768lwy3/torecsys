3095407c5ab0af27033e772b777a9dea9306002c
Jasper <jasper@ztore.com>
2019-08-30 16:08:51 +0800

    Update: setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,51 @@
-from setuptools import setup
+from io import open
+from os import path
+from setuptools import setup, find_packages
+
+# set rootdir to the repository root directory
+rootdir = path.abspath(path.dirname(__file__))
+
+# read readme.md to long_description
+with open(path.join(rootdir, "README.md"), encoding="utf-8") as readme:
+    long_description = readme.read()
+
 
 setup(
+    # Required: project name
     name    = "torecsys",
-    version = "0.0.1-dev"
+    # Required: tag
+    version = "0.0.1-dev",
+    # Optional: long description
+    long_description=long_description,
+    # Optional: long description type
+    long_description_content_type = "text/markdown",
+    # Optional: project url
+    url = "https://github.com/p768lwy3/torecsys",
+    # Optional: author
+    author = "Jasper Li",
+    # Optional: author email
+    author_email = "jasper.li.wy@gmail.com",
+    # Classifier
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7"
+    ],
+    # Optional: keywords
+    keywords = "recommendationsystem machinelearning research",
+    # Required: packages
+    packages = find_packages(exclude=["contrib", "docs", "tests"]),
+    # Optional: install_required
+    install_required = ["torch"],
+    # Optional: extras_required 
+    # extras_required = {},
+    # Optional: extra project url
+    projects_url = {
+        "Bug Reports": "https://github.com/p768lwy3/torecsys/issues",
+        "Documentation": "https://github.com/p768lwy3/torecsys",
+        "Source": "https://github.com/p768lwy3/torecsys"
+    }
 )
+
