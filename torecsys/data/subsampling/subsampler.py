f92c25a07df9bd9582ee0f3fd2196cd71c615a0b
Jasper <p768lwy3@gmail.com>
2019-12-24 16:12:43 +0800

    update metrics, subsampler and readme

--- torecsys/data/subsampling/subsampler.py
+++ torecsys/data/subsampling/subsampler.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pandas as pd
+from typing import Union
 
-
-def subsampling(array     : np.ndarray, 
-                column_id : int   = 0, 
+def subsampling(data      : Union[np.ndarray, pd.DataFrame], 
+                key       : Union[int, str], 
                 threshold : float = 1e-2) -> np.ndarray:
     r"""Drop occurrences of the most frequent word tokens with the following condition by the given threshold value:
     if :math:`P_{\text{random}} < P_{\text{drop}}`, where :math:`P_{\text{random}} \in U(0, 1)` and :math:`P_{drop} = 1 - \sqrt{\frac{threshold}{frequencies}}`
@@ -26,25 +27,43 @@ def subsampling(array     : np.ndarray,
     #. `Omer Levy et al, 2015. Improving Distributional Similarity with Lessons Learned from Word Embeddings <https://levyomer.files.wordpress.com/2015/03/improving-distributional-similarity-tacl-2015.pdf>`_.
 
     """
+    # initialize columns' selector with lambda function
+    if isinstance(data, np.ndarray):
+        row_func = lambda data, row_key: data[row_key]
+        col_func = lambda data, col_key: data[:, col_key]
+        val_func = lambda data, row_key, col_key: data[row_key, col_key]
+    elif isinstance(data, pd.DataFrame):
+        row_func = lambda data, row_key: data.iloc[row_key]
+        
+        if isinstance(key, int):
+            col_func = lambda data, col_key: data.iloc[:, col_key]
+            val_func = lambda data, row_key, col_key: data.iloc[row_key, col_key]
+        else:
+            col_func = lambda data, col_key: data[col_key]
+            val_func = lambda data, row_key, col_key: data.loc[row_key, col_key]
+
+    # select columns from data
+    columns = col_func(data, key)
+    
     # count occurences of each token
-    uniques, counts = np.unique(array[:, column_id], return_counts=True)
+    uniques, counts = np.unique(columns, return_counts=True)
     
     # calculate the frequencies of each token
-    frequencies = counts / np.sum(counts)
+    freq = counts / np.sum(counts)
     
     # calculate the subsampling probabilities
-    probabilities = 1 - np.sqrt(threshold / frequencies)
+    prob = 1 - np.sqrt(threshold / freq)
     
     # created subsampling probabilities dict
-    probabilities = dict(zip(uniques, probabilities))
+    prob = dict(zip(uniques, prob))
     
     # generate random values for each row of samples
-    random_probabilities = np.random.random(size=(array.shape[0]))
+    rand_prob = np.random.random(size=(data.shape[0]))
     
     # subsampling
     subsampled_data = []
-    for i in range(array.shape[0]):
-        if random_probabilities[i] < probabilities[array[i, column_id]]:
-            subsampled_data.append(array[i])
+    for i in range(data.shape[0]):
+        if rand_prob[i] < prob[val_func(data, i, key)]:
+            subsampled_data.append(row_func(data, i))
     
     return np.vstack(subsampled_data)
