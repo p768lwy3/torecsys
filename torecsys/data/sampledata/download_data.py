1d56bf0ed18e3d681133f9b35b5e16c1028517e3
Jasper <p768lwy3@gmail.com>
2020-01-06 16:14:37 +0800

    adding sample data: criteo for experment testing

--- torecsys/data/sampledata/download_data.py
+++ torecsys/data/sampledata/download_data.py
@@ -13,8 +13,8 @@ with warnings.catch_warnings():
 
 def download_ml_data(size : str,
                      dir  : str = None):
-    r"""Download movielens data from grouplens to directory ../sample_data/,
-    and the source url of movielens is : https://grouplens.org/datasets/movielens/
+    r"""Download movielens data from grouplens.
+    Source: https://grouplens.org/datasets/movielens/.
     
     Args:
         size (str): Movielens dataset size, allows: 20m, latest-small, latest, 100k, 1m, 10m
@@ -69,3 +69,53 @@ def download_ml_data(size : str,
 
     print("Finished: file %s is downloaded to the directory: %s" % 
           (unzip_folderdir, os.path.join(samples_dir, unzip_folderdir)))
+
+def download_criteo_data(dir: str = None):
+    r"""Download criteo data from Critel AI Lab.
+    Source: https://labs.criteo.com/2014/02/download-kaggle-display-advertising-challenge-dataset/.
+    
+    Args:
+        dir (str, optional): Directory to save downloaded data. Defaults to None.
+    """
+    # set directory name and create directory if not exist
+    if dir is None:
+        script_dir = os.path.dirname(__file__)
+        samples_dir = os.path.join(script_dir, "sample_data")
+    else:
+        samples_dir = dir
+    Path(samples_dir).mkdir(parents=True, exist_ok=True)
+
+    # set file name to download data
+    unzip_folderdir = "dac"
+    zip_filename = "dac.tar.gz"
+    zip_fileurl = "https://s3-eu-west-1.amazonaws.com/kaggle-display-advertising-challenge-dataset/dac.tar.gz"
+    zip_fileloc = os.path.join(samples_dir, zip_filename)
+
+    # streaming to iterate over the response
+    r = requests.get(zip_fileurl, stream=True)
+
+    # total size in bytes
+    total_size = int(r.headers.get("content-length", 0))
+    block_size = 1024
+    wrote = 0
+
+    # save the file
+    with open(zip_fileloc, "wb") as f:
+        for data in tqdm(r.iter_content(block_size), 
+                         total=math.ceil(total_size / block_size),
+                         unit="KB",
+                         unit_scale=True):
+            wrote += len(data)
+            f.write(data)
+    
+    if total_size !=0 and wrote != total_size:
+        raise RuntimeError("something went wrong.")
+    
+    # unzip and remove the file
+    with zipfile.ZipFile(zip_fileloc, "r") as zip_file:
+        zip_file.extractall(samples_dir)
+    os.remove(zip_fileloc)
+
+    print("Finished: file %s is downloaded to the directory: %s" % 
+          (unzip_folderdir, os.path.join(samples_dir, unzip_folderdir)))
+    
--- torecsys/data/sampledata/load_data.py
+++ torecsys/data/sampledata/load_data.py
@@ -1,10 +1,11 @@
 from . import __ml_size__
 import pandas as pd
 import os
+from typing import Tuple
 
 def load_ml_data(size : str,
-                 dir  : str = None) -> pd.DataFrame:
-    r"""Load movielens dataset from ml-[size]/ratings.csv file in ../sample_data to pandas.DataFrame.
+                 dir  : str = None) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    r"""Load movielens dataset from dir/ml-[size] to pd.DataFrame.
     
     Args:
         size (str): Movielens dataset size, allows: 20m, latest-small, latest, 100k, 1m, 10m
@@ -14,7 +15,7 @@ def load_ml_data(size : str,
         ValueError: when size is not in allowed values 
     
     Returns:
-        pd.DataFrame: movielens dataset dataframe, with columns = [userId, movieId, rating, timestamp]
+        Tuple[df, df, df, df]: Tuple of movielens dataset dataframe
     """
     # check if the size is allowed
     if size not in __ml_size__:
@@ -40,3 +41,34 @@ def load_ml_data(size : str,
     tags_df = pd.read_csv(tags_path)
 
     return links_df, movies_df, ratings_df, tags_df
+
+def load_criteo_data(dir: str = None) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Load criteo dataset from dir/dac to pd.DataFrame.
+    
+    Args:
+        dir (str, optional): Directory to save downloaded data. Default to None.
+    
+    Returns:
+        Tuple[df, df]: Tuple of criteo click-thorugh-rate dataframe, with 39 columns.
+    """
+    # set directory name of the downloaded data:
+    if dir is None:
+        script_dir = os.path.dirname(__file__)
+        samples_dir = os.path.join(script_dir, "sample_data")
+    else:
+        samples_dir = dir
+
+    # set file path to dac
+    train_path = os.path.join(samples_dir, ("dac/train.txt"))
+    test_path = os.path.join(samples_dir, ("dac/test.txt"))
+    
+    columns = ["col_%s" % str(i) for i in range(0, 40)]
+
+    with open(train_path, "r") as train_txt:
+        train_file = pd.read_csv(train_txt, sep="\t", names=columns)
+    
+    with open(test_path, "r") as test_txt:
+        test_file = pd.read_csv(test_txt, sep="\t", names=columns)
+    
+    return train_file, test_file
+    
